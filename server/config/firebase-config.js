64b45283f3fc88e154ea89f1f6206ed482e647a0
surya <ranasurya436@gmail.com>
2026-09-15 02:09:51 +0530

    Support Firebase secret file on Render

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,11 +1,25 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
+import fs from "fs";
 
-import serviceAccountKey from "./serviceAccountKey.json" with { type: "json" };
+const secretFilePath = "/etc/secrets/serviceAccountKey.json";
+
+const localFilePath = new URL(
+  "./serviceAccountKey.json",
+  import.meta.url
+);
+
+const serviceAccountKey = JSON.parse(
+  fs.readFileSync(
+    fs.existsSync(secretFilePath) ? secretFilePath : localFilePath,
+    "utf-8"
+  )
+);
 
 const app = initializeApp({
   credential: cert(serviceAccountKey),
 });
 
 const auth = getAuth(app);
+
 export default auth;
